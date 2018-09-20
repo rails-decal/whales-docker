5db8990b41f8d962ddb2bff68fde928e958f6f17
Glitch (bright-patch) <none>
2018-09-20 16:48:55 +0000

    🍷💏 Updated with Glitch

---
+++ app/services/weather_service.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+
+class WeatherService
+  def self.get(city)
+    url = URI.parse(
+      "http://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV['WEATHER']}"
+    )
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    data = JSON.parse res.body
+    {
+      temperature: data["main"]["temp"],
+      description: data["weather"][0]["description"],
+      city: data["name"]
+    }
+  end
+end
